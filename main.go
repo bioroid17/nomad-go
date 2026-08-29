de7f6f6bdec0e61e481daa729ce3cf72032b7802
bioroid17 <trace1997@ajou.ac.kr>
2026-08-29 17:13:11 +0900

    Theory - Maps

--- main.go
+++ main.go
@@ -3,7 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	names := []string{"Nico", "Lynn", "Dal"}
-	names = append(names, "flynn")
-	fmt.Println(names)
+	nico := map[string]string {
+		"name": "Nico",
+		"age": "12",
+	}
+	for key, value := range nico {
+		fmt.Println(key, value)
+	}
 }

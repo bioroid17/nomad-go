e6bee8008d70c15f50d1c07d51d2341286469a5a
bioroid17 <trace1997@ajou.ac.kr>
2026-08-29 17:38:27 +0900

    Theory - Structs

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import "fmt"
 
+type person struct {
+	name	string
+	age		int
+	favFood []string
+}
+
 func main() {
-	nico := map[string]string {
-		"name": "Nico",
-		"age": "12",
+	favFood := []string{"pizza", "burgers"}
+	nico := person{
+		name: "nico",
+		age:  18,
+		favFood: favFood,
 	}
-	for key, value := range nico {
-		fmt.Println(key, value)
-	}
+	fmt.Println(nico.name, nico.age, nico.favFood)
 }

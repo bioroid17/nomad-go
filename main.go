6b75daa1a6e383de47e0a18ed11d55a5a18dc5ce
bioroid17 <trace1997@ajou.ac.kr>
2026-09-03 00:52:44 +0900

    Bank & Dictionary Projects - Methods part 2

--- main.go
+++ main.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"nomad-go/accounts"
 )
 
 func main() {
 	account := accounts.NewAccount("nico")
 	account.Deposit(10)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(20)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	fmt.Println(account.Balance())
 }

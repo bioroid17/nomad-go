dafe8c799c092626455b91504aaeef780cbd2c8f
bioroid17 <trace1997@ajou.ac.kr>
2026-09-05 01:24:42 +0900

    Bank & Dictionary Projects - Update Delete

--- mydict/mydict.go
+++ mydict/mydict.go
@@ -5,8 +5,11 @@ import "errors"
 // Dictionary type
 type Dictionary map[string]string
 
-var errNotFound = errors.New("Not Found")
-var errWordExists = errors.New("Word already exists")
+var (
+	errNotFound = errors.New("Not Found")
+	errWordExists = errors.New("Word already exists")
+	errCantUpdate = errors.New("Cannot update non-existing word")
+)
 
 // Search for a word
 func (d Dictionary) Search(word string) (string, error) {
@@ -27,4 +30,19 @@ func (d Dictionary) Add(word, def string) error {
 		return errWordExists
 	}
 	return nil
+}
+
+func (d Dictionary) Update(word, def string) error {
+	_, err := d.Search(word)
+	switch err {
+	case nil:
+		d[word] = def
+	case errNotFound:
+		return errCantUpdate
+	}
+	return nil
+}
+
+func (d Dictionary) Delete(word string) {
+	delete(d, word)
 }

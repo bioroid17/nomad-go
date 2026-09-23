ecaded7ea3c94ba3f739af6b3443c4fe21be7611
bioroid17 <trace1997@ajou.ac.kr>
2026-09-24 01:34:55 +0900

    URL Checker & Go Routines - Slow URLChecker

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 var errRequestFailed = errors.New("request failed")
 
 func main() {
+	var results = make(map[string]string)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -21,7 +22,15 @@ func main() {
 		"https://academy.nomadcoders.co/",
 	}
 	for _, url := range urls {
-		hitURL(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 

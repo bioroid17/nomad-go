40896dc76921a506578931bc3b1af5daa5960a69
bioroid17 <trace1997@ajou.ac.kr>
2026-09-26 02:49:51 +0900

    Job Scrapper - getPages part 2

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -10,9 +12,18 @@ import (
 var baseUrl = "https://www.saramin.co.kr/zf_user/search/recruit?searchword=python"
 
 func main() {
-	getPages()
+	totalPages := getPages()
+	
+	for i := 0; i < totalPages; i++ {
+		getPage(i)
+	}
+}
+func getPage(page int) {
+	pageUrl := baseUrl + "&recruitPage=" + strconv.Itoa(page + 1)
+	fmt.Println("Requesting", pageUrl)
 }
 func getPages() int {
+	pages := 0
 	res, err := http.Get(baseUrl)
 	checkErr(err)
 	checkCode(res)
@@ -22,9 +33,11 @@ func getPages() int {
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 	checkErr(err)
 
-	doc.Find(".pagination")
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		pages = s.Find("a").Length()
+	})
 
-	return 0
+	return pages
 }
 
 func checkErr(err error) {

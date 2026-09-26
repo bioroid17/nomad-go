a3faaa9a46ceb39077d6175a49e1a38a2eab034d
bioroid17 <trace1997@ajou.ac.kr>
2026-09-26 22:24:29 +0900

    Job Scrapper - extractJob part 1

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 	"github.com/PuerkitoBio/goquery"
 )
 
+type extractedJob struct {
+	id string
+	title string
+	location string
+	salary string
+}
+
 var baseUrl = "https://www.saramin.co.kr/zf_user/search/recruit?searchword=python"
 
 func main() {
@@ -21,6 +28,26 @@ func main() {
 func getPage(page int) {
 	pageUrl := baseUrl + "&recruitPage=" + strconv.Itoa(page + 1)
 	fmt.Println("Requesting", pageUrl)
+	res, err := http.Get(pageUrl)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	jobCards := doc.Find(".item_recruit")
+	jobCards.Each(func(i int, card *goquery.Selection) {
+		id, _ := card.Attr("value")
+		fmt.Println(id)
+		title := card.Find(".area_job > .job_tit > a").Text()
+		fmt.Println(title)
+		location := card.Find(".area_job > .job_condition > span:first-child").Text()
+		fmt.Println(location)
+		salary := card.Find(".area_job > .job_condition > span:nth-child(5)").Text()
+		fmt.Println(salary)
+	})
 }
 func getPages() int {
 	pages := 0
